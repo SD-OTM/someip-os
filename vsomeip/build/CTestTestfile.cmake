abdd87db3e5c823b39c2f4d514dabebccf64aabd
SD-OTM <otmane007.os0@gmail.com>
2024-08-28 14:52:33 +0200

    add build folder

---
+++ vsomeip/build/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/os/Desktop/someip-os/vsomeip
+# Build directory: /home/os/Desktop/someip-os/vsomeip/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("examples/routingmanagerd")
+subdirs("tools")
+subdirs("examples")
+subdirs("test")
---
+++ vsomeip/build/test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/os/Desktop/someip-os/vsomeip/test
+# Build directory: /home/os/Desktop/someip-os/vsomeip/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
